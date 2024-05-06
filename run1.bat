2e7ca57a4c90353da2dceaaede47732d9a37c0e8
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-05-06 07:47:53 +0700

    fix: run GUI

---
+++ run1.bat
@@ -0,0 +1,9 @@
+cd src
+javac -d ..\bin *.java
+@REM xcopy dictionary.txt ..\bin\ /Y
+@REM xcopy word_dictionary.txt ..\bin\ /Y
+@REM xcopy words_alpha.txt ..\bin\ /Y
+xcopy dictionary.txt ..\bin\ /Y
+cd ..
+cd bin
+java WordLadderGUI
