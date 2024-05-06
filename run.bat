81a47b88240ee7aa201108d288c41e92f5811615
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-05-06 07:47:45 +0700

    fix: run main

--- run.bat
+++ run.bat
@@ -1,9 +1,6 @@
 cd src
 javac -d ..\bin *.java
-@REM xcopy dictionary.txt ..\bin\ /Y
-@REM xcopy word_dictionary.txt ..\bin\ /Y
-@REM xcopy words_alpha.txt ..\bin\ /Y
-xcopy newdict.txt ..\bin\ /Y
+xcopy dictionary.txt ..\bin\ /Y
 cd ..
 cd bin
-java WordLadderGUI
+java Main
